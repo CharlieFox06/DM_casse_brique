efacbdf61a7e949357fc577940b42ad4f8e052f0
charliefox06 <117631018+CharlieFox06@users.noreply.github.com>
2022-12-03 21:50:42 +0100

    Update and rename main109.py to main110.py

--- main110.py
+++ main110.py
@@ -35,11 +35,9 @@ def balle_deplacement(x, y):
 
 """
 def balle_deplacement(x, y):
-    x += dx
-    y += dy
-    if (y <= bord_gauche) or (y >= bord_droite):
+    if y <= bord_gauche or y >= bord_droite:
         dy = -dy
-    if (x <= bord_haut):
+    if x <= bord_haut:
         dx = -dx
     return x, y
 
