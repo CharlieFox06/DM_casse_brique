28fccb90fe15bb2701ca7d4633fce43419a77fe7
charliefox06 <117631018+CharlieFox06@users.noreply.github.com>
2022-12-03 12:54:17 +0100

    Update and rename main63.py to main64.py

--- main64.py
+++ main64.py
@@ -27,13 +27,12 @@ def vaisseau_deplacement(x, y):
     return x, y
 
 def balle_deplacement(x, y):
-    if pyxel.btnr(pyxel.KEY_SPACE):
-        x += dx
-        y += dy
-        if y <= bord_gauche or y >= bord_droite:
-            dy = -dy
-        if x <= bord_haut:
-            dx = -dx
+    x += dx
+    y += dy
+    if y <= bord_gauche or y >= bord_droite:
+        dy = -dy
+    if x <= bord_haut:
+        dx = -dx
     return x, y
 
         
