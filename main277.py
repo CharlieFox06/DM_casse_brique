0dbb4ed4fa79c41d0d21e081a33cb0d97f612e51
charliefox06 <117631018+CharlieFox06@users.noreply.github.com>
2022-12-11 22:19:20 +0100

    Update and rename main276.py to main277.py

--- main277.py
+++ main277.py
@@ -52,50 +52,50 @@ def balle_deplacement(x, y, dx, dy, brique_1, brique_2, brique_3, brique_4, briq
     if y <= bord_haut + r:
         dy = -dy   
     if brique_1 == True:
-        if x == b_1_x and b_y + (r+5) <= y <= b_y + 4 + (r+5):
+        if x == b_1_x and b_y <= y <= b_y + 4:
             dx = -dx
-        elif x == b_1_x + 16 and b_y + (r+5) <= y <= b_y + 4 + (r+5):
+        elif x == b_1_x + 16 + (r+5) and b_y <= y <= b_y + 4:
             dx = -dx
         elif y == b_y and b_1_x <= x <= (b_1_x + 16):
             dy = -dy
         elif y == b_y + 4 + (r+5) and b_1_x <= x <= (b_1_x + 16):
             dy = -dy
     if brique_2 == True:
-        if x == b_2_x and b_y + (r+5) <= y <= b_y + 4 + (r+5):
+        if x == b_2_x and b_y <= y <= b_y + 4:
             dx = -dx
-        elif x == b_2_x + 16 and b_y + (r+5) <= y <= b_y + 4 + (r+5):
+        elif x == b_2_x + 16 + (r+5) and b_y <= y <= b_y + 4:
             dx = -dx
         elif y == b_y and b_2_x <= x <= (b_2_x + 16):
             dy = -dy
         elif y == b_y + 4 + (r+5) and b_2_x <= x <= (b_2_x + 16):
             dy = -dy
     if brique_3 == True:
-        if x == b_3_x and b_y + (r+5) <= y <= b_y + 4 + (r+5):
+        if x == b_3_x and b_y <= y <= b_y + 4:
             dx = -dx
-        elif x == b_3_x + 16 and b_y + (r+5) <= y <= b_y + 4 + (r+5):
+        elif x == b_3_x + 16 + (r+5) and b_y <= y <= b_y + 4:
             dx = -dx
         elif y == b_y and b_3_x <= x <= (b_3_x + 16):
             dy = -dy
         elif y == b_y + 4 + (r+5) and b_3_x <= x <= (b_3_x + 16):
             dy = -dy
     if brique_4 == True:
-        if x == b_4_x and b_y + (r+5) <= y <= b_y + 4 + (r+5):
+        if x == b_4_x and b_y <= y <= b_y + 4:
             dx = -dx
-        elif x == b_4_x + 16 and b_y + (r+5) <= y <= b_y + 4 + (r+5):
+        elif x == b_4_x + 16 + (r+5) and b_y <= y <= b_y + 4:
             dx = -dx
         elif y == b_y and b_4_x <= x <= (b_4_x + 16):
             dy = -dy
         elif y == b_y + 4 + (r+5) and b_4_x <= x <= (b_4_x + 16):
             dy = -dy
     if brique_5 == True:
-        if x == b_5_x and b_y + (r+5) <= y <= b_y + 4 + (r+5):
+        if x == b_5_x and b_y <= y <= b_y + 4:
             dx = -dx
-        elif x == b_5_x + 16 and b_y + (r+5) <= y <= b_y + 4 + (r+5):
+        elif x == b_5_x + 16 + (r+5) and b_y <= y <= b_y + 4:
             dx = -dx
         elif y == b_y and b_5_x <= x <= (b_5_x + 16):
             dy = -dy
         elif y == b_y + 4 + (r+5) and b_5_x <= x <= (b_5_x + 16):
-            dy = -dy     
+            dy = -dy    
     return x, y, dx, dy, brique_1, brique_2, brique_3, brique_4, brique_5, b_y, b_1_x, b_2_x, b_3_x, b_4_x, b_5_x
 
 def bounce_off_vaisseau(x, y, dx, dy, vaisseau_x, vaisseau_y):
