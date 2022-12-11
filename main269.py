870be1b8d9f90e784ea47cbafa80d15d4c69868b
charliefox06 <117631018+CharlieFox06@users.noreply.github.com>
2022-12-11 22:04:32 +0100

    Update and rename main268.py to main269.py

--- main269.py
+++ main269.py
@@ -56,45 +56,45 @@ def balle_deplacement(x, y, dx, dy, brique_1, brique_2, brique_3, brique_4, briq
             dx = -dx
         elif x == b_1_x + 16 and b_y <= y <= b_y + 4:
             dx = -dx
-        elif y == b_y - (r+1) and b_1_x <= x <= (b_1_x + 16):
+        elif y == b_y + (r+1) and b_1_x <= x <= (b_1_x + 16):
             dy = -dy
-        elif y == b_y + 4 - (r+1) and b_1_x <= x <= (b_1_x + 16):
+        elif y == b_y + 4 + (r+1) and b_1_x <= x <= (b_1_x + 16):
             dy = -dy
     if brique_2 == True:
         if x == b_2_x and b_y <= y <= b_y + 4:
             dx = -dx
         elif x == b_2_x + 16 and b_y <= y <= b_y + 4:
             dx = -dx
-        elif y == b_y - (r+1) and b_2_x <= x <= (b_2_x + 16):
+        elif y == b_y + (r+1) and b_2_x <= x <= (b_2_x + 16):
             dy = -dy
-        elif y == b_y + 4 - (r+1) and b_2_x <= x <= (b_2_x + 16):
+        elif y == b_y + 4 + (r+1) and b_2_x <= x <= (b_2_x + 16):
             dy = -dy
     if brique_3 == True:
         if x == b_3_x and b_y <= y <= b_y + 4:
             dx = -dx
         elif x == b_3_x + 16 and b_y <= y <= b_y + 4:
             dx = -dx
-        elif y == b_y - (r+1) and b_3_x <= x <= (b_3_x + 16):
+        elif y == b_y + (r+1) and b_3_x <= x <= (b_3_x + 16):
             dy = -dy
-        elif y == b_y + 4 - (r+1) and b_3_x <= x <= (b_3_x + 16):
+        elif y == b_y + 4 + (r+1) and b_3_x <= x <= (b_3_x + 16):
             dy = -dy
     if brique_4 == True:
         if x == b_4_x and b_y <= y <= b_y + 4:
             dx = -dx
         elif x == b_4_x + 16 and b_y <= y <= b_y + 4:
             dx = -dx
-        elif y == b_y - (r+1) and b_4_x <= x <= (b_4_x + 16):
+        elif y == b_y + (r+1) and b_4_x <= x <= (b_4_x + 16):
             dy = -dy
-        elif y == b_y + 4 - (r+1) and b_4_x <= x <= (b_4_x + 16):
+        elif y == b_y + 4 + (r+1) and b_4_x <= x <= (b_4_x + 16):
             dy = -dy
     if brique_5 == True:
         if x == b_5_x and b_y <= y <= b_y + 4:
             dx = -dx
         elif x == b_5_x + 16 and b_y <= y <= b_y + 4:
             dx = -dx
-        elif y == b_y - (r+1) and b_5_x <= x <= (b_5_x + 16):
+        elif y == b_y + (r+1) and b_5_x <= x <= (b_5_x + 16):
             dy = -dy
-        elif y == b_y + 4 - (r+1) and b_5_x <= x <= (b_5_x + 16):
+        elif y == b_y + 4 + (r+1) and b_5_x <= x <= (b_5_x + 16):
             dy = -dy     
     return x, y, dx, dy, brique_1, brique_2, brique_3, brique_4, brique_5, b_y, b_1_x, b_2_x, b_3_x, b_4_x, b_5_x
 
