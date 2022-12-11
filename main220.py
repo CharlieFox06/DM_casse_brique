4ec16e23cee216c0fe0f72cc565b5725bc24f84e
charliefox06 <117631018+CharlieFox06@users.noreply.github.com>
2022-12-11 15:28:00 +0100

    Update and rename main219.py to main220.py

--- main220.py
+++ main220.py
@@ -19,16 +19,12 @@ bord_gauche = 0
 bord_droite = 128
 score = 0
 r = 4
+b_y = 40
 b_1_x = 22
-b_1_y = 40
 b_2_x = 39
-b_2_y = 40
 b_3_x = 56
-b_3_y = 40
 b_4_x = 73
-b_4_y = 40
 b_5_x = 90
-b_5_y = 40
 brique_1 = True
 brique_2 = True
 brique_3 = True
@@ -53,34 +49,38 @@ def balle_deplacement(x, y, dx, dy):
         dx = -dx
     if y <= bord_haut + r:
         dy = -dy
-    
+        
+    return x, y, dx, dy
+
+def bounce_off_briques(x, y, dx, dy, brique_1, brique_2, brique_3, brique_4, brique_5, b_y, b_1_x, b_2_x, b_3_x, b_4_x, b_5_x):
     if brique_1 == True:
-        if b_1_x <= x <= (b_1_x + 16):
+        if b_1_x <= x <= (b_1_x + 16) and ((y == b_y) or (y == (b_y + 4))):
             dx = -dx
-        if b_1_y <= y <= (b_1_y + 4):
+        if b_y <= y <= (b_y + 4) and ((x== b_1_x) or (x== (b_1_x + 16))):
             dy = -dy
     if brique_2 == True:
-        if b_2_x <= x <= (b_1_x + 16):
+        if b_2_x <= x <= (b_2_x + 16) and ((y == b_y) or (y == (b_y + 4))):
             dx = -dx
-        if b_2_y <= y <= (b_1_y + 4):
+        if b_y <= y <= (b_y + 4) and ((x== b_2_x) or (x== (b_2_x + 16))):
             dy = -dy
     if brique_3 == True:
-        if b_3_x <= x <= (b_1_x + 16):
+        if b_3_x <= x <= (b_3_x + 16) and ((y == b_y) or (y == (b_y + 4))):
             dx = -dx
-        if b_3_y <= y <= (b_1_y + 4):
+        if b_y <= y <= (b_y + 4) and ((x== b_3_x) or (x== (b_3_x + 16))):
             dy = -dy
     if brique_4 == True:
-        if b_4_x <= x <= (b_1_x + 16):
+        if b_4_x <= x <= (b_4_x + 16) and ((y == b_y) or (y == (b_y + 4))):
             dx = -dx
-        if b_4_y <= y <= (b_1_y + 4):
+        if b_y <= y <= (b_y + 4) and ((x== b_4_x) or (x== (b_4_x + 16))):
             dy = -dy
     if brique_5 == True:
-        if b_5_x <= x <= (b_1_x + 16):
+        if b_5_x <= x <= (b_5_x + 16) and ((y == b_y) or (y == (b_y + 4))):
             dx = -dx
-        if b_5_y <= y <= (b_1_y + 4):
+        if b_y <= y <= (b_y + 4) and ((x== b_5_x) or (x== (b_5_x + 16))):
             dy = -dy
-    
-    return x, y, dx, dy
+            
+    return x, y, dx, dy, brique_1, brique_2, brique_3, brique_4, brique_5, b_y, b_1_x, b_2_x, b_3_x, b_4_x, b_5_x
+
 
 def bounce_off_vaisseau(x, y, dx, dy, vaisseau_x, vaisseau_y):
     """ rebondi sur le haut du vaisseau """
@@ -124,6 +124,8 @@ def update():
     # mise a jour de la position de la balle
     balle_x, balle_y, dx, dy = balle_deplacement(balle_x, balle_y, dx, dy)
     balle_x, balle_y, dx, dy, vaisseau_x, vaisseau_y = bounce_off_vaisseau(balle_x, balle_y, dx, dy, vaisseau_x, vaisseau_y)
+    balle_x, balle_y, dx, dy, brique_1, brique_2, brique_3, brique_4, brique_5, b_y, b_1_x, b_2_x, b_3_x, b_4_x, b_5_x = bounce_off_briques(balle_x, balle_y, dx, dy, brique_1, brique_2, brique_3, brique_4, brique_5, b_y, b_1_x, b_2_x, b_3_x, b_4_x, b_5_x):
+
     
     # mise a jour du score (30 par seconde)
     score = score_timer(score)
