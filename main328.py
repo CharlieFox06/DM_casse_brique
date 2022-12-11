e86cdee7a9b77afb410673a497d6a6500bd0cd19
charliefox06 <117631018+CharlieFox06@users.noreply.github.com>
2022-12-12 00:45:11 +0100

    Update and rename main327.py to main328.py

--- main328.py
+++ main328.py
@@ -65,15 +65,15 @@ def balle_deplacement(x, y, dx, dy, brique_1, brique_2, brique_3, brique_4, briq
         elif x == b_1_x + 16 + (r+5) and b_y <= y <= b_y + 4 + (r+1):
             dx = -dx
             brique_1 = False
-            score += 100
+            score += 200
         elif y == b_y and b_1_x <= x <= (b_1_x + 16):
             dy = -dy
             brique_1 = False
-            score += 100
+            score += 300
         elif y == b_y + 4 + (r+5) and b_1_x <= x <= (b_1_x + 16):
             dy = -dy
             brique_1 = False
-            score += 100
+            score += 200
     if brique_2 == True:
         if x == b_2_x and b_y <= y <= b_y + 4 + (r+1):
             dx = -dx
